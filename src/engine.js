be3c5e0f38872a49e9a2ffcbe1bdeebfc211e069
nullberg <nullberg@imap.cc>
2022-12-05 18:03:24 +0300

    Remove unnecessary for loop

--- src/engine.js
+++ src/engine.js
@@ -3,24 +3,23 @@ import readlineSync from 'readline-sync';
 const maxWins = 3;
 
 const play = (name, gameDescription, getTask) => {
-  for (let scores = 0; scores < maxWins;) {
-    console.log(gameDescription);
+  let scores = 0;
+  console.log(gameDescription);
 
-    while (scores < maxWins) {
-      const task = getTask();
-      console.log(`Question: ${task.question}`);
-      const userAnswer = readlineSync.question('Your answer: ');
-      if (userAnswer.toString() === task.answer.toString()) {
-        console.log('Correct!');
-        scores += 1;
-      } else {
-        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${task.answer}'.`);
-        console.log(`Let's try again, ${name}!`);
-        return null;
-      }
+  while (scores < maxWins) {
+    const task = getTask();
+    console.log(`Question: ${task.question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer.toString() === task.answer.toString()) {
+      console.log('Correct!');
+      scores += 1;
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${task.answer}'.`);
+      console.log(`Let's try again, ${name}!`);
+      return null;
     }
-    console.log(`Congratulations, ${name}!`);
   }
+  console.log(`Congratulations, ${name}!`);
   return null;
 };
 
